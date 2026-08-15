3785db202c23e8c4bde9ec7f591f1337f0736ab3
Ashritha <ashritha1255@gmail.com>
2026-08-15 18:14:49 +0530

    Palindrome

---
+++ 1Basics/_3Palindrome.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class _3Palindrome{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int x = n;
+        
+        int rev = 0;
+        while(n>0){
+            int last = n%10;
+            rev = rev*10 + last;
+            n/=10;
+        }
+        if(x==rev){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+    }
+}
